366161d4f4763911f642ebe9e0b33a94e002ddc4
hdd <comehere.hdd@gmail.com>
2024-10-29 18:56:18 +0700

    folder structure

---
+++ client/src/components/header/header.jsx
@@ -0,0 +1,36 @@
+import React, { useEffect, useRef, useState } from 'react';
+import { Link, NavLink } from 'react-router-dom';
+import { BiMenu } from 'react-icons/bi';
+
+
+let navLinks = []
+
+let account = []
+const guessNavLinks = [
+  {
+    path: '/home',
+    display: 'Home',
+  },
+
+]
+
+const clientNavLinks = [
+  {
+    path: '/home',
+    display: 'Home',
+  },
+]
+
+const managerNavLinks = [
+  {
+    path: '/home',
+    display: 'Home',
+  },
+]
+
+
+const Header = () => {
+   
+  };
+  
+  export default Header;
