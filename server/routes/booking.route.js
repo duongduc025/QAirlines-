79394404c23e4eb9ea612f337ef7ba9b8ec02c9a
Tnemo65 <ledacthinh666@gmail.com>
2024-12-04 04:20:42 +0700

    Define role action

--- server/routes/booking.route.js
+++ server/routes/booking.route.js
@@ -1,15 +1,16 @@
 import express from 'express';
 import { getBookingsByUserEmail, getSpecificBookingByUserEmail, createBooking, listAllBookings, cancelBooking } from '../controllers/booking.controller.js';
 import { authenticateJWT } from '../middlewares/jwtAuth.js';
+import { isAdmin, isCustomer } from '../middlewares/auth.middleware.js';
 
 const router = express.Router();
 
 // ...existing code...
 
-router.get('/bookingslist', listAllBookings); 
-router.get('/users/:email/bookings', authenticateJWT, getBookingsByUserEmail);
-router.get('/users/:email/bookings/:booking_id', authenticateJWT, getSpecificBookingByUserEmail);
-router.post('/bookings', authenticateJWT, createBooking);
-router.delete('/bookings/:booking_id', authenticateJWT, cancelBooking);
+router.get('/bookingslist',  authenticateJWT, isAdmin, listAllBookings); 
+router.get('/users/:email/bookings', authenticateJWT, isCustomer, getBookingsByUserEmail);
+router.get('/users/:email/bookings/:booking_id', authenticateJWT, isCustomer, getSpecificBookingByUserEmail);
+router.post('/bookings', authenticateJWT, isCustomer, createBooking);
+router.delete('/bookings/:booking_id', authenticateJWT, isCustomer, cancelBooking);
 
 export default router;
--- server/routes/flights.route.js
+++ server/routes/flights.route.js
@@ -1,15 +1,15 @@
 import express from 'express';
 import { showAllFlights, addNewFlight, searchFlights, updateDepartureTime } from '../controllers/flights.controller.js';
-import { isAdmin } from '../middlewares/auth.middleware.js';
+import { isAdmin, isCustomer } from '../middlewares/auth.middleware.js';
 import { authenticateJWT } from '../middlewares/jwtAuth.js';
 
 const router = express.Router();
 
 // ...existing code...
 
-router.get('/allFlights', showAllFlights);
-router.post('/addFlight', authenticateJWT, addNewFlight);
-router.get('/search', searchFlights);
+router.get('/allFlights', authenticateJWT, isAdmin, showAllFlights);
+router.post('/addFlight', authenticateJWT, isAdmin, addNewFlight);
+router.get('/search', authenticateJWT, isCustomer, searchFlights);
 router.patch('/updateDepartureTime/:flightId', authenticateJWT, isAdmin, updateDepartureTime);
 
 // ...existing code...
